f3ba8d84cc3bbcc774928149889035ec0b69ab07
LWProgramming <lwgithub@gmail.com>
2021-01-15 18:34:55 -0500

    implement reading from questions

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
+from typing import Any
+
+from pandas import Series
+
 from app import app
 import pandas as pd
+
+
+current_question_id = 0
+previous_questions = {}  # map previous question id to previous answers.
+df = pd.read_csv('questions.csv')
+
+
+def get_cell_contents_from_single_row(row: Series, column_name: str) -> Any:
+    return row.iloc[0][column_name]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    data = pd.read_csv('questions.csv')
-    return str(data)
+    current_question_row = df.loc[df['question_id'] == current_question_id]
+    current_question_text = get_cell_contents_from_single_row(current_question_row, 'question_text')
+    return current_question_text
