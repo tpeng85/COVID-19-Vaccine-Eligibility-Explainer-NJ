bf98d6a8b994dc0063f82f1f9cb62282c56fd09d
LWProgramming <lwgithub@gmail.com>
2021-01-15 19:02:47 -0500

    Implement basic questionnaire user flow

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from typing import Any
 
-from pandas import Series
+from flask import render_template, request
+from pandas import Series, DataFrame
 
 from app import app
 import pandas as pd
 
 
 current_question_id = 0
-previous_questions = {}  # map previous question id to previous answers.
+previous_responses = {}  # map previous question texts to previous answers.
 df = pd.read_csv('questions.csv')
 
 
@@ -15,9 +16,38 @@ def get_cell_contents_from_single_row(row: Series, column_name: str) -> Any:
     return row.iloc[0][column_name]
 
 
-@app.route('/')
-@app.route('/index')
+def get_row_from_question_id(df: DataFrame, question_id: str) -> Series:
+    return df.loc[df['question_id'] == question_id]
+
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    current_question_row = df.loc[df['question_id'] == current_question_id]
-    current_question_text = get_cell_contents_from_single_row(current_question_row, 'question_text')
-    return current_question_text
+    global current_question_id
+    current_question_row = get_row_from_question_id(df, current_question_id)
+    current_question_text = get_cell_contents_from_single_row(
+        current_question_row, 'question_text')
+
+    if request.method == 'POST':
+        assert len(list(request.form.values())) == 1
+        user_answer = list(request.form.values())[0] # string "Yes", "No", or "Unsure"
+        previous_responses[current_question_text] = user_answer
+        current_question_id = get_cell_contents_from_single_row(current_question_row, user_answer + '_response_next')
+        # Need to update the other information now that we have a new current question
+        if current_question_id < 0:
+            # need to exit this loop and immediately go to an ending screen
+            return 'hi'
+        current_question_row = get_row_from_question_id(df, current_question_id)
+        current_question_text = get_cell_contents_from_single_row(
+            current_question_row, 'question_text')
+
+    more_information = get_cell_contents_from_single_row(
+        current_question_row,
+        'more information')
+    yes_response_text = get_cell_contents_from_single_row(
+        current_question_row,
+        'yes_response_text')
+    return render_template('index.html',
+                           current_question_text=current_question_text,
+                           yes_response_text=yes_response_text,
+                           more_information=more_information,
+                           previous_responses=previous_responses)
